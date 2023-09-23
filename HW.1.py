9db2808d07fbbdd973e72ddc2676240e7d180623
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-23 10:33:00 +0300

    Update HW.1.py

--- HW.1.py
+++ HW.1.py
@@ -1,9 +1,8 @@
-a = int(input())
-b = int(input())
-c = int(input())
-f = int(input())
-d = int(input())
-if (f - d) == 0:
-    print("Делить на ноль нельзя!")
+x = float(input())
+y = float(input())
+if x > y:
+    print("1-ое число больше 2-ого")
+elif x == y:
+    print("Числа равны")
 else:
-    k = (a * b - c) / (f - d)
+    print("2-ое число больше 1-ого")
