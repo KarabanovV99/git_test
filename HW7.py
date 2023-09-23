0557213c76030ca8f7611c207eb8ad0145d94021
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-23 10:52:37 +0300

    Update HW7.py

--- HW7.py
+++ HW7.py
@@ -1,7 +1,8 @@
+import math
 x = float(input())
 y = float(input())
 c = 0
-for i in range(int(x), int(y) + 1):
+for i in range(math.ceil(x), math.floor(y) + 1):
     if i % 5 == 0:
         c += i
 print(c)
