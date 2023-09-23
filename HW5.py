ba797f75227b5d8d302255ba3672fd1318b9b6cb
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-23 10:46:02 +0300

    Update HW5.py

--- HW5.py
+++ HW5.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 x = float(input())
 y = float(input())
 if x == 0 or y == 0:
@@ -13,21 +12,5 @@ elif x < 0 and y < 0:
     print("3 четверть")
 elif x == 0:
     print("ось ординат")
-elif y == 0:
-    print("ось абсцис")
-=======
-x = int(input())
-y = int(input())
-if x == 0 or y == 0:
-    print("точка лежит на оси")
-elif x > 0 and y > 0:
-    print("1 четверть")
-elif x < 0 and y > 0:
-    print("2 четверть")
-elif x > 0 and y < 0:
-    print("4 четверть")
-elif x < 0 and y < 0:
-    print("3 четверть")
 else:
-    print("erorr")
->>>>>>> d763727929bf4d2ac5acea7ebe8e8adeb8287758
+    print("ось абсцис")
