19e73357ba6fc2e4b61310be1735923eb3e1282e
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-23 10:43:57 +0300

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a = float(input())
 b = float(input())
 c = float(input())
@@ -8,15 +7,4 @@ if f - d == 0:
     print("Делить на ноль нельзя!")
 else:
     k = (a * b - c) / (f - d)
-=======
-a = int(input())
-b = int(input())
-c = int(input())
-f = int(input())
-d = int(input())
-if (f - d) == 0:
-    print("Делить на ноль нельзя!")
-else:
-    k = (a * b - c) / (f - d)
->>>>>>> d763727929bf4d2ac5acea7ebe8e8adeb8287758
-    print(k)
+print(k)
